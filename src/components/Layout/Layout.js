960e319075e07ce0a01ca23c598af4f22d810809
unknown <mohankumar.be.cse@gmail.com>
2021-01-12 13:48:47 +1300

    implement BurgerBilder container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import Auxiliary from '../../hoc/Auxiliary';
+import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
-    <Auxiliary>
+    <Aux>
         <div>Toolbar, SideDrawer, BackDrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
-    </Auxiliary>
+    </Aux>
    
 );
 
