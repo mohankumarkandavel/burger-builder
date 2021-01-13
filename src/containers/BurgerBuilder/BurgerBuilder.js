164494b89a5c7d726d1691da975e306ba4675f23
unknown <mohankumar.be.cse@gmail.com>
2021-01-13 15:09:39 +1300

    remove ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -33,12 +33,36 @@ class BurgerBuilder extends Component {
         this.setState({totaPrice: newPrice,  ingredients: updateIngredients});
     };
 
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if(oldCount <= 0){
+            return;
+        }
+        const updateCount = oldCount - 1;
+        const updateIngredients = {
+            ...this.state.ingredients
+        };
+        updateIngredients[type] = updateCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totaPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({totaPrice: newPrice,  ingredients: updateIngredients});
+    };
+
     render() {
+        const disableInfo = {
+            ...this.state.ingredients
+        }
+        for(let key in disableInfo) {
+            disableInfo[key] = disableInfo[key] <= 0
+        }
         return (
             <Aux>
                 <Burger ingredients= {this.state.ingredients}/>
                 <BuildControls 
-                    ingredientAdded={this.addIngredientHandler}/>
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disableInfo}/>
             </Aux>
         );
     }
