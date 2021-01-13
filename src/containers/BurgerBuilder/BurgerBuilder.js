a12d9a7f6c5aaa205489eb999e76341908bb780d
unknown <mohankumar.be.cse@gmail.com>
2021-01-13 13:11:45 +1300

    calculate ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,10 +5,10 @@ import Aux from '../../hoc/Auxiliary';
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
-            meat: 2,
-            cheese: 2
+            salad: 0,
+            bacon: 0,
+            meat: 0,
+            cheese: 0
         }
     }
     render() {
